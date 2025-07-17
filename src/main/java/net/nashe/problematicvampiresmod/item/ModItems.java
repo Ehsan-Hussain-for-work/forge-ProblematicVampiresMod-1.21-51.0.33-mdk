56cbd8eb41d099046bf64605e54bb2df66ef7752
Ehsan Hussain <ehsanhussain899@gmail.com>
2025-07-17 15:07:41 -0400

    Added Sunstone and it's dust. With a good enough texter.

---
+++ src/main/java/net/nashe/problematicvampiresmod/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.nashe.problematicvampiresmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.nashe.problematicvampiresmod.ProblematicVampiresMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, ProblematicVampiresMod.MOD_ID);
+
+    public static final RegistryObject<Item> SUNSTONE = ITEMS.register("sunstone", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> SUNSTONE_DUST = ITEMS.register("sunstone_dust", () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
